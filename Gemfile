15f9ae19444344b7fb5e01fea0ffa030714fe761
Muhammad Ramzan <124147803+progammer743@users.noreply.github.com>
2023-03-20 19:49:19 +0500

    sign up modified with tailwind

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "activestorage", "~> 6.1"
 gem "devise", "~> 4.9"
+
+gem "tailwindcss-rails", "~> 2.0"
---
+++ config/tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'background-image': "url('background1.jpeg')"
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "gold": "#d97706",
+        "gold_shade2": "#b45309",
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
