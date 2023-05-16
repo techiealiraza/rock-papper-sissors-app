deb91fa5ac17b721d33edb4bdde2f465084f279d
Ali Raza <124589600+techiealiraza@users.noreply.github.com>
2023-05-16 19:56:50 +0500

    Update index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -8,8 +8,3 @@ application.load(definitionsFromContext(context));
 import CountdownController from "./countdown_controller";
 application.register("countdown", CountdownController);
 
-import PassController from "./pass_controller";
-application.register("pass", PassController);
-
-import ChoiceController from "./choice_controller";
-application.register("choice", ChoiceController);
