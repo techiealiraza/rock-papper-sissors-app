f718e97990eba138d9aaeba409d2242259424ec9
Muhammad Ramzan <124147803+progammer743@users.noreply.github.com>
2023-05-22 18:37:19 +0500

    refactored code

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -4,17 +4,17 @@ class Tournament < ApplicationRecord
   paginates_per 3
   has_many :tournaments_users
   has_many :users, through: :tournaments_users
+  belongs_to :winner, class_name: 'User', foreign_key: 'tournament_winner_id', optional: true
   has_many :matches, dependent: :destroy
   has_one_attached :image
-  validates :name, :description, :start_date, :end_date, :registration_deadline, presence: true, on: :create
-  validate :end_date_is_after_start_date, on: :create
-  validate :start_date_is_after_end_date, on: :create
-  validate :start_date_validation, on: :create
-  validate :deadline_before_start_date, on: :create
+  validates :name, :description, :start_date, :end_date, :registration_deadline, presence: true
+  validate :end_date_is_after_start_date
+  validate :start_date_validation
+  validate :deadline_before_start_date
 
   def deadline_before_start_date
     return if registration_deadline.nil? || start_date.nil?
-    return unless registration_deadline > start_date || registration_deadline < Time.now
+    return unless registration_deadline > start_date || registration_deadline < Time.zone.now
 
     errors.add(:registration_deadline, 'Registration Deadline is after Start Date')
   end
@@ -26,42 +26,39 @@ class Tournament < ApplicationRecord
     errors.add(:end_date, 'End Date is before Start Date')
   end
 
-  def start_date_is_after_end_date
-    return if end_date.nil? || start_date.nil?
-    return unless start_date > end_date
-
-    errors.add(:start_date, 'Start Date is after End Date')
-  end
-
   def start_date_validation
     return if start_date.nil?
-    return unless start_date < Time.now
+    return unless start_date < Time.zone.now
 
     errors.add(:start_date, 'Start Date is in Past')
   end
 
-  def match_winners_count(round)
-    matches.where.not(match_winner_id: nil).where(round:)
+  def done_matches_size(round)
+    matches.by_round(round).where.not(match_winner_id: nil).size
   end
 
-  def matches_by_round(round)
-    matches.where(round:)
+  def remaining_matches_by_round_size(round)
+    matches.by_round(round).where(match_winner_id: nil).size
+  end
+
+  def matches_by_round_size(round)
+    matches.by_round(round).size
   end
 
   def current_round_winners(round)
-    ids = matches.where.not(match_winner_id: nil).where(round:).pluck(:match_winner_id)
-    User.where(id: ids)
-  end
-
-  def update_winner(tournament_winner_id)
-    update(tournament_winner_id:)
+    users.where(id: matches.select(:match_winner_id).by_round(round))
   end
 
   def current_match_time
     if matches.empty?
       start_date + 30.seconds
     else
-      Time.now + 90.seconds
+      Time.zone.now + 90.seconds
     end
+  end
+
+  def create_matches(match)
+    MatchCreator.new(self, current_round_winners(match.round),
+                     match.round + 1).create_match
   end
 end
