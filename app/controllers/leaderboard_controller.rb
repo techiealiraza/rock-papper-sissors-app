87b1e9ba5c7f0bef8d50f22a85c409d58478e63d
Muhammad Ramzan <124147803+progammer743@users.noreply.github.com>
2023-05-31 19:56:38 +0500

    leaderboard query optimized + leaderboard responsiveness added

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,6 +1,6 @@
 # LeaderBoard Controller
 class LeaderboardController < ApplicationController
   def index
-    @users = User.members
+    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -46,6 +46,22 @@ class User < ApplicationRecord
     tournaments.distinct.where(tournament_winner_id: id).size
   end
 
+  def leader_board_records
+    data = []
+    User.all.each do |user|
+      tournaments_won = user.total_tournaments_won
+      data << { user:, tournaments_won: }
+    end
+    top_tournaments_winners = data.sort_by! { |record| record[:tournaments_won] }.reverse!.take(10)
+    top_tournaments_winners.each do |record|
+      matches_played = record[:user].total_matches_played
+      tournaments_played = record[:user].total_tournaments_played
+      matches_won = record[:user].total_matches_won
+      record.merge!({ matches_played:, tournaments_played:, matches_won: })
+    end
+    top_tournaments_winners
+  end
+
   def member?
     role == 'member'
   end
