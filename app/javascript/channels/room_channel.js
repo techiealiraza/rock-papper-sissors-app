b8cfa44752209d5b73c3a7bb6e01feefa22824c4
Ali Raza <124589600+techiealiraza@users.noreply.github.com>
2023-06-02 18:38:52 +0500

    Pr points fixed
    ability
    room_channel
    countdown controller

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -21,33 +21,33 @@ document.addEventListener("turbolinks:load", () => {
         );
         let messageHTML;
         if (data.user_name === current_user_name) {
-          messageHTML = `<div class = "my-1 flex flex-row">
-      <div class = "rounded-full bg-white w-7 text-black text-center h-7">
-      ${data.user_name.substring(0, 3)}
-      </div>
-      <div class ="flex flex-col space-y-2 text-xs  ml-2 items-start">
-			<div class="line-clamp-10 max-w-sm text-lg px-3 py-2 my-1 rounded-lg rounded-br-none rounded-tl-none bg-white text-black">
-        ${data.message}
-        </div>
-      <div class = "flex flex-row items-end">
-        <div class = "text-white">${data.created_at}</div>
-      </div>
-      </div>
-   </div>`;
+          messageHTML = `<div class="my-1 flex flex-row">
+                         <div class="rounded-full bg-white w-7 text-black text-center h-7">
+                         ${data.user_name.substring(0, 3)}
+                         </div>
+                         <div class ="flex flex-col space-y-2 text-xs  ml-2 items-start">
+			                   <div class="line-clamp-10 max-w-sm text-lg px-3 py-2 my-1 rounded-lg rounded-br-none rounded-tl-none bg-white text-black">
+                         ${data.message}
+                         </div>
+                         <div class="flex flex-row items-end">
+                         <div class="text-white">${data.created_at}</div>
+                         </div>
+                         </div>
+                         </div>`;
         } else {
-          messageHTML = `<div class = "my-1 flex flex-row">
-      <div class = "rounded-full bg-gold_shade2 w-7 text-white text-center h-7">
-      ${data.user_name.substring(0, 3)}
-      </div>
-      <div class ="flex flex-col space-y-2 text-xs  ml-2 items-start">
-			<div class="line-clamp-10 max-w-sm text-lg px-3 py-2 my-1 rounded-lg rounded-br-none rounded-tl-none bg-gold text-black">
-        ${data.message}
-      </div>
-      <div class = "flex flex-row items-end">
-        <div class = "text-white">${data.created_at}</div>
-      </div>
-      </div>
-   </div>`;
+          messageHTML = `<div class="my-1 flex flex-row">
+                         <div class="rounded-full bg-gold_shade2 w-7 text-white text-center h-7">
+                         ${data.user_name.substring(0, 3)}
+                         </div>
+                         <div class="flex flex-col space-y-2 text-xs  ml-2 items-start">
+                         <div class="line-clamp-10 max-w-sm text-lg px-3 py-2 my-1 rounded-lg rounded-br-none rounded-tl-none bg-gold text-black">
+                         ${data.message}
+                         </div>
+                         <div class="flex flex-row items-end">
+                         <div class="text-white">${data.created_at}</div>
+                         </div>
+                         </div>
+                         </div>`;
         }
 
         msgs.insertAdjacentHTML("afterbegin", messageHTML);
