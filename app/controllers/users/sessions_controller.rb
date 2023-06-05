0c55f340d640aed575e5fc019d5246e18b33e755
Ali Raza <124589600+techiealiraza@users.noreply.github.com>
2023-06-05 17:23:51 +0500

    session fixed

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@ module Users
   class SessionsController < Devise::SessionsController
     before_action :otp_generate_and_send, only: [:create]
     before_action :authenticate_user!, except: %i[new create destroy]
-    def otp_redirect
+    def verify_otp
       authenticate_2fa!
     end
 
--- db/migrate/20230605114240_change_match_winner_id_name_in_match.rb
+++ db/migrate/20230605114240_change_match_winner_id_name_in_match.rb
@@ -1,5 +1,5 @@
 class ChangeMatchWinnerIdNameInMatch < ActiveRecord::Migration[6.1]
   def change
-    rename_column :matches, :winner_id, :winner_id
+    rename_column :matches, :match_winner_id, :winner_id
   end
 end
