058b23800b9601e112a89ecf63e7bebc7e14e3c7
Muhammad Ramzan <124147803+progammer743@users.noreply.github.com>
2023-06-05 17:45:28 +0500

    removed params & aincludes winner

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -4,7 +4,7 @@ class TournamentsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @tournaments = Tournament.includes(%i[users]).all
+    @tournaments = Tournament.includes(%i[users winner]).all
     @tournaments = @tournaments.order(:registration_deadline).page(params[:page])
   end
 
