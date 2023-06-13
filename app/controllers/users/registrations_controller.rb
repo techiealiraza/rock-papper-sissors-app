45b855e404c2825139112433e9dd31483718b883
Ali Raza <124589600+techiealiraza@users.noreply.github.com>
2023-06-13 13:39:04 +0500

    remove unused code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -27,9 +27,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(_resource)
     root_path
   end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[image name phone_number])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[image name phone_number])
-  end
 end
